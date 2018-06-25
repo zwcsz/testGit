a0847ef1f198156cd51e616b8ec570192dc6793d
我真的不只会吃 <461712598@qq.com>
2018-06-26 00:49:57 +0800

    0:49

--- src/main/java/team/zwcsz/gradeManagement/controller/Test.java
+++ src/main/java/team/zwcsz/gradeManagement/controller/Test.java
@@ -4,11 +4,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
+
 		System.out.println(" world helllo！");
-=======
-		System.out.println(" world h！");
->>>>>>> refs/remotes/origin/master
 	}
 
 }

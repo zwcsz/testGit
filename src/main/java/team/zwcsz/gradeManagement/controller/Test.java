2e1f713e2d11a58b816e3832c5f26836669edb4e
我真的不只会吃 <461712598@qq.com>
2018-06-25 16:50:38 +0800

    First Commit

---
+++ src/main/java/team/zwcsz/gradeManagement/controller/Test.java
@@ -0,0 +1,10 @@
+package team.zwcsz.gradeManagement.controller;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("测试成功！");
+	}
+
+}
